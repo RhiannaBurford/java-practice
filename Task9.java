d048da51440ae7ccd766da9d8fbe4f170b06a8f7
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-01 18:03:42 +0100

    Loop practice

---
+++ Task9.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args){
+        /*Write a Java application that inputs a series of 10 integers and determines 
+        and prints the largest integer */
+
+        // Initialise variables
+        Scanner sinput = new Scanner(System.in);
+        int[] nums = new int[10];
+        
+
+        // Gather user input
+        for (int i = 0; i < nums.length; i++){
+            System.out.print("Enter number "+ (i+1) + ": ");
+            nums[i] = sinput.nextInt();
+        }
+
+        int max = nums[0];
+        // Find largest integer
+        for (int value : nums){
+            if (value > max){
+                max = value;
+            }
+        }
+
+        System.out.println("The maximum is "+ max);
+        sinput.close();
+    }
+}
