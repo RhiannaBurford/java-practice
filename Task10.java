9c859b650e64a8cafe7655a70657f299a3810f48
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-01 18:21:05 +0100

    counter manipulation

--- Task10.java
+++ Task10.java
@@ -1,9 +1,10 @@
 public class Task10 {
     public static void main(String[] args){
         /* Printing specific table of values */
-
+        System.out.println("N    10*N    100*N    1000*N");
         for (int i = 1; i < 6; i++){
-            System.out.println(i + "    " + (i*10) + "    " + (i*100) + "    " + (i*1000));
+            System.out.printf("%-5d %-5d %-5d %-5d%n", i, i*10, i*100, i*1000);
+
         }
     }
 }
