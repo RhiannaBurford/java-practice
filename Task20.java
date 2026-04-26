449cff8ecd7db05733cd9e0422e73102a42f9860
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-26 08:40:49 +0100

    Read 'using your answer to the above question' for question 2

--- Task20.java
+++ Task20.java
@@ -12,4 +12,26 @@ public class Task20 {
 
         return count;
     }
+
+
+    public char maxTwoString(String s1, String s2) {
+    // We need to keep track of the best one we've found so far
+    char maxChar = ' '; 
+    int maxCount = -1;
+
+    for (int i = 0; i < s2.length(); i++) {
+        char currentChar = s2.charAt(i);
+        
+        // 1. USE the method from part (a)
+        int currentCount = numOccurrences(s1, currentChar);
+
+        // 2. Check if this is the new "winner"
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            maxChar = currentChar;
+        }
+    }
+
+    return maxChar;
+}
 }
