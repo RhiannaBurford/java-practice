f7239dd25ceda4ec81da2a532306829298f8cf6f
Rhianna Burford <rhiannaburford@hotmail.co.uk>
2026-04-27 11:00:15 +0100

    Remember to add a constructor with no arguments

---
+++ IllegalMoveException.java
@@ -0,0 +1,10 @@
+public class IllegalMoveException extends Exception {
+
+    public IllegalMoveException(){
+        super("Exception");
+    }
+
+    public IllegalMoveException(String message){
+        super(message);
+    }
+}
